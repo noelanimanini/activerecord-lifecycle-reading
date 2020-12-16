0b91a4b34aaa5bcdf5a277f8af18f2ffde1646e9
smolnreadytobrawl <noelani.villanueva@gmail.com>
2020-12-16 10:38:02 -0800

    feet are weird

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ActiveRecord::Base
 
   belongs_to :author
   validate :is_title_case 
-
+  before_validation :make_title_case
   private
 
   def is_title_case
@@ -15,3 +15,4 @@ class Post < ActiveRecord::Base
     self.title = self.title.titlecase
   end
 end
+
